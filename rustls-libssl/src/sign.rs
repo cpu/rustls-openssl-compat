d1d901145dc7bf91985a1ba032ea79939d39e5bc
Daniel McCarney <daniel@binaryparadox.net>
2024-07-16 13:32:37 -0400

    implement X509_check_private_key
    Rustls 0.23.11 added a `keys_match()` function to `CertifiedKey`
    instances. This commit uses that new capability to implement
    `X509_check_private_key()`.
    
    Following the precedent set upstream in Rustls we're permissive if
    we can't determine a key's SPKI and return `C_INT_SUCCESS` in this case.

--- rustls-libssl/src/sign.rs
+++ rustls-libssl/src/sign.rs
@@ -88,19 +88,39 @@ impl CertifiedKeySet {
 }
 
 #[derive(Clone, Debug)]
-struct OpenSslCertifiedKey {
+pub(super) struct OpenSslCertifiedKey {
     key: EvpPkey,
     openssl_chain: OwnedX509Stack,
     rustls_chain: Vec<CertificateDer<'static>>,
 }
 
 impl OpenSslCertifiedKey {
-    fn new(chain: Vec<CertificateDer<'static>>, key: EvpPkey) -> Result<Self, error::Error> {
+    pub(super) fn new(
+        chain: Vec<CertificateDer<'static>>,
+        key: EvpPkey,
+    ) -> Result<Self, error::Error> {
         Ok(Self {
             key,
             openssl_chain: OwnedX509Stack::from_rustls(&chain)?,
             rustls_chain: chain,
         })
+    }
+
+    pub(super) fn keys_match(&self) -> bool {
+        match sign::CertifiedKey::new(
+            self.rustls_chain.clone(),
+            Arc::new(OpenSslKey(self.key.clone())),
+        )
+        .keys_match()
+        {
+            // Note: we allow "Unknown" to be treated as success here. This is returned
+            //   when it wasn't possible to get the SPKI for the private key, and so we
+            //   aren't certain if it matches or not.
+            Ok(()) | Err(rustls::Error::InconsistentKeys(rustls::InconsistentKeys::Unknown)) => {
+                true
+            }
+            _ => false,
+        }
     }
 
     fn borrow_cert(&self) -> *mut X509 {
